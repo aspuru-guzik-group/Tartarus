ca534c66a4e0c2c21bf58cb4d8eb82cb918336e7
John Willes <johnwilles@gmail.com>
2023-06-23 13:19:11 -0400

    Update benchmark and example

--- example.py
+++ example.py
@@ -6,19 +6,8 @@ from tartarus import reactivity
 # test and print all objecties
 dipm, gap, lumo, combined, pce_pcbm_sas, pce_pcdtbt_sas = pce.get_properties('c1sc(-c2[SiH2]c(cc2)-c2ccc(-c3scc4occc34)c3cscc23)c2Cccc12')
 print('******* PCE *******')
-print(f'Dipole: {dipm}')
-print(f'HOMO-LUMO Gap: {gap}')
-print(f'LUMO: {lumo}')
-print(f'Combined obj: {combined}')
 print(f'PCE1: {pce_pcbm_sas}')
 print(f'PCE2: {pce_pcdtbt_sas}')
-print()
-
-dipm, gap, lumo, combined = pce.get_surrogate_properties('c1sc(-c2[SiH2]c(cc2)-c2ccc(-c3scc4occc34)c3cscc23)c2Cccc12')
-print(f'Dipole: {dipm}')
-print(f'HOMO-LUMO Gap: {gap}')
-print(f'LUMO: {lumo}')
-print(f'Combined obj: {combined}')
 print()
 
 st, osc, combined = tadf.get_properties('O=C1NC2=C(O1)C1=C(N=CO1)C2=O')
@@ -28,7 +17,7 @@ print(f'Oscillator strength: {osc}')
 print(f'Combined obj: {combined}')
 print()
 
-score = docking.get_1syh_score('Cl/C(=C/Cl)/CC(N)C(=O)O')
+score = docking.perform_calc_single('Cl/C(=C/Cl)/CC(N)C(=O)O', '1syh', docking_program='qvina')
 print('******* Docking *******')
 print(f'Docking score: {score}')
 print()
